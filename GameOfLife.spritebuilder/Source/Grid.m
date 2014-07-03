9691d0dccf30b2f35be173a24440a5674f36b74b
AayushB <Aayush@purpamine.bh>
2014-07-02 19:41:05 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -12,11 +12,11 @@
 static const int GRID_ROWS = 8;
 static const int GRID_COLUMNS = 10;
 
-@implementation Grid {
+@implementation Grid
     NSMutableArray *_gridArray;
     float _cellWidth;
     float _cellHeight;
-}
+
 
 - (void)onEnter
 {
