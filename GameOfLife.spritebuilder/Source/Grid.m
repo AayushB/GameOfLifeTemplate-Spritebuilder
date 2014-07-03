0b7a3493f18af3e7da53f200859bf40d594a1ffe
AayushB <Aayush@purpamine.bh>
2014-07-02 19:10:18 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -157,15 +157,13 @@ static const int GRID_COLUMNS = 10;
             
             if(currentCreature.livingNeighbors==3)
             {
-                //currentCreature.isAlive=YES;
-                [currentCreature setIsAlive:YES];
-                NSLog(@"ok");
+                currentCreature.isAlive=YES;
+                //[currentCreature setIsAlive:YES];
             }
             else if(currentCreature.livingNeighbors <=1 || currentCreature.livingNeighbors >=4)
             {
-                //currentCreature.isAlive=NO;
-                 [currentCreature setIsAlive:NO];
-                NSLog(@"not ok");
+                currentCreature.isAlive=NO;
+                // [currentCreature setIsAlive:NO];
             }
         }
     }
